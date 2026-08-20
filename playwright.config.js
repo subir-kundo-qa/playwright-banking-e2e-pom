324bbdc9dc5685d132e9e530e75f76adb33052b1
Subir Kundu <83028666+subirkundu@users.noreply.github.com>
2026-08-20 13:28:55 +0600

    Clean up playwright.config.js
    Removed TypeScript check and documentation comment.

--- playwright.config.js
+++ playwright.config.js
@@ -1,9 +1,4 @@
-// @ts-check
 import { defineConfig, devices } from '@playwright/test';
-
-/**
- * @see https://playwright.dev/docs/test-configuration
- */
 export default defineConfig({
   testDir: './tests',
   timeout: 40 * 1000, // Global timeout for each test (40s)
